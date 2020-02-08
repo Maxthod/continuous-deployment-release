41ee9416c949a111d1bed2e97045bae178c31def
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-07 19:09:49 -0500

    Continue...

--- src/com/duvalhub/release/parameters/Parameters.groovy
+++ src/com/duvalhub/release/parameters/Parameters.groovy
@@ -8,14 +8,22 @@ class Parameters {
 
     Parameters(String git_repository, String flow_type, String version) {
         this.git_repository = git_repository
-        this.flow_type = flow_type.toUpperCase() as FlowType
-        this.version = version.toUpperCase() as Version
+        this.flow_type = FlowType.fromtString(flow_type)
+        this.version = Version.fromString(version)
     }
 }
 
 enum FlowType {
     RELEASE, PRODUCTION
     FlowType(){}
+        public static FlowType fromString(String text) {
+        for (FlowType b : FlowType.values()) {
+            if (b.text.equalsIgnoreCase(text)) {
+                return b;
+            }
+        }
+        return null;
+    }
 }
 
 enum Version {
@@ -23,4 +31,13 @@ enum Version {
     MINOR,
     MAJOR
     Version() {}
+
+    public static Version fromString(String text) {
+        for (Version b : Version.values()) {
+            if (b.text.equalsIgnoreCase(text)) {
+                return b;
+            }
+        }
+        return null;
+    }    
 }
