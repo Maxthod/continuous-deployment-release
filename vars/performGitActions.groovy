aadb043f51b191c994540eb9fdd850fa8d4a6921
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-08 18:08:01 -0500

    Continue...

--- vars/performGitActions.groovy
+++ vars/performGitActions.groovy
@@ -2,7 +2,9 @@ import com.duvalhub.release.performgitactions.PerformGitActions
 
 def call(PerformGitActions performGitActions) {
     dir( performGitActions.app_workdir ) {
-        String script = "${env.WORKSPACE}/scripts/gitaction/gitaction.sh"
-        executeScript(script)        
+        withShhKey() {
+            String script = "${env.WORKSPACE}/scripts/gitaction/gitaction.sh"
+            executeScript(script)        
+        }
     }
 }
