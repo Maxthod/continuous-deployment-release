2b4dbc43998200ed1926661b12acf55708e5ef1c
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-08 18:14:42 -0500

    Continue...

--- scripts/gitaction/gitaction.sh
+++ scripts/gitaction/gitaction.sh
@@ -4,7 +4,7 @@
 pwd
 ls -l
 git remote -v
-export GIT_SSH_COMMAND="ssh -oStrictHostKeyChecking=accept-new -i $SSH_KEY_PATH -F /dev/null"
+#export GIT_SSH_COMMAND="ssh -oStrictHostKeyChecking=accept-new -i $SSH_KEY_PATH -F /dev/null"
 NEW_BRANCH="release/1.2.3"
 git checkout -b $NEW_BRANCH
 git push origin $NEW_BRANCH
