2c0f1ea79f511e1a66d92ce51f098c1661cb92d9
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-02-08 11:16:37 -0500

    Continue...

--- src/com/duvalhub/release/parameters/Parameters.groovy
+++ src/com/duvalhub/release/parameters/Parameters.groovy
@@ -13,7 +13,7 @@ class Parameters {
     }
 }
 
-public static enum FlowType {
+enum FlowType {
     RELEASE, PRODUCTION
     FlowType(){}
         public static FlowType fromString(String text) {
@@ -26,7 +26,7 @@ public static enum FlowType {
     }
 }
 
-static enum Version {
+enum Version {
     PATCH,
     MINOR,
     MAJOR
