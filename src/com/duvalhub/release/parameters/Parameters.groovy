783af2da045c066fe3ccfd02fa3000f7994cc673
Maxthod <maxthod@pm.me>
2022-03-19 20:21:33 -0400

    Continue...

--- src/com/duvalhub/release/parameters/Parameters.groovy
+++ src/com/duvalhub/release/parameters/Parameters.groovy
@@ -13,12 +13,12 @@ class Parameters {
         this.dry_run = dry_run
     }
 
-    Parameters(Map params) {
-        this.git_repository = params.GIT_REPOSITORY
-        this.flow_type = params.FLOW_TYPE
-        this.version = params.VERSION
-        this.dry_run = params.DRY_RUN
-    }
+//    Parameters(Map params) {
+//        this.git_repository = params.GIT_REPOSITORY
+//        this.flow_type = params.FLOW_TYPE
+//        this.version = params.VERSION
+//        this.dry_run = params.DRY_RUN
+//    }
 
     Boolean isDryRun() {
         return this.git_repository == '' || this.flow_type == '' || this.version == '' || dry_run == 'true'
