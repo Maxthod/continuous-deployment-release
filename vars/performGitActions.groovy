1c3b87b6c1c70407742a7ebeb2290809a6fb8fc7
Maxthod <maxthod@pm.me>
2022-04-04 23:23:50 -0400

    Continue...

--- vars/performGitActions.groovy
+++ vars/performGitActions.groovy
@@ -28,7 +28,7 @@ def call(PerformGitActions performGitActions) {
 
                         docker.image(image)
                             .inside() { c ->
-                                sh "cat"
+                                sh "sleep 50000"
                                 new_version = executeScript(version_script, true)
                             }
                         env.NEW_VERSION = new_version
